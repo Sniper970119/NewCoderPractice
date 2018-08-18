1b8fe494b296c997b2faf7b6e7a0fabce051ca5b
Zhao <30463691+Sniper970119@users.noreply.github.com>
2018-08-18 22:23:42 +0800

    updata

--- src/SeriesRestore.java
+++ src/SeriesRestore.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 /**
  * Created by Sniper on 2018/8/18.
  */
 public class SeriesRestore {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[] data = new int[n+1];
+        int[] book = new int[11];
+        for (int i = 0; i < n; i++)
+        {
+            data[i] = scanner.nextInt();
+            book[data[i]] = 1;
+        }
+
 
     }
 }
