734a073bf2a23b8c14dbd52f20289a5606664f6c
Zhao <30463691+Sniper970119@users.noreply.github.com>
2018-08-18 21:41:28 +0800

    updata

--- src/FindPrecious.java
+++ src/FindPrecious.java
@@ -1,5 +1,41 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.Scanner;
+
 /**
  * Created by Sniper on 2018/8/18.
  */
 public class FindPrecious {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String t = scanner.nextLine();
+        String s = scanner.nextLine();
+        int length = s.length();
+        int flag = 0;
+        if (findSub(t,s)){
+            System.out.println("Yes");
+        }else
+        System.out.println("No");
+    }
+    private static boolean findSub(String a,String b){
+//        System.out.println("string:"+a+" "+b);
+        if (b.length()==0){
+            return true;
+        }
+        if (a.length()==0){
+            return false;
+        }
+        for (int i = 0; i < a.length(); i++)
+        {
+           if ( b.charAt(0) == a.charAt(i)){
+//               System.out.println(b.charAt(0)+" "+a.charAt(i));
+               if (findSub(a.substring(i+1),b.substring(1))){
+                   return true;
+               }
+               return false;
+           }
+
+        }
+        return false;
+    }
 }
