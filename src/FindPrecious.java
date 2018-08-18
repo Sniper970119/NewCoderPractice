85d944dccdf82f2ef6f8711efeae883aeae60eb1
Zhao <30463691+Sniper970119@users.noreply.github.com>
2018-08-18 21:43:11 +0800

    updata

--- src/FindPrecious.java
+++ src/FindPrecious.java
@@ -17,18 +17,21 @@ public class FindPrecious {
         }else
         System.out.println("No");
     }
+    /**
+     * 递归寻找子字符串是否匹配
+     * */
     private static boolean findSub(String a,String b){
-//        System.out.println("string:"+a+" "+b);
+        // 判断两个字符串是否为空，为空则返回true或者false
         if (b.length()==0){
             return true;
         }
         if (a.length()==0){
             return false;
         }
+        // 判断字符串中是否含有目标字符串中字符串，如果有，裁剪字符串递归调用
         for (int i = 0; i < a.length(); i++)
         {
            if ( b.charAt(0) == a.charAt(i)){
-//               System.out.println(b.charAt(0)+" "+a.charAt(i));
                if (findSub(a.substring(i+1),b.substring(1))){
                    return true;
                }
