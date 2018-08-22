517bd82f4aa38da7847844faf064db0114a98fff
Zhao <30463691+Sniper970119@users.noreply.github.com>
2018-08-22 21:03:21 +0800

    updata

--- src/TheWordsYiLike.java
+++ src/TheWordsYiLike.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * Created by Sniper on 2018/8/22.
+ * Created by Sniper on 2018/8/22..
  */
 public class TheWordsYiLike {
     public static void main(String[] args){
@@ -17,23 +17,24 @@ public class TheWordsYiLike {
     /**
      * 判断是否符合第一个条件
      * */
-    public static boolean firstCondition(String line){
+    private static boolean firstCondition(String line){
         // 匹配大写字母
         return line.matches("[A-Z]+");
     }
     /**
      * 判断是否符合第二个条件
      * */
-    public static boolean secondCondition(String line){
+    private static boolean secondCondition(String line){
         // match是从起始位置检查，不像find或者findall那种，可以在中间
         // 所以前后都要加上.*
-        //检查有没有两个连续的字符， 第一个
+        //检查有没有两个连续的字符， 第一个获取任意字符，第二个是和前面.相同的字母，类似于SQL预处理中的 ?
         return !line.matches(".*(.)(\\1).*");
     }
     /**
      * 判断是否符合第三个条件
      * */
-    public static boolean thirdCondition(String line){
+    private static boolean thirdCondition(String line){
+        // 判断是否会有子串重复（xyxy类型）
         return !line.matches(".*(.).*(.)(.*\\1)(.*\\2).*");
     }
 }
