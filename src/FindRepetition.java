7704870bec0e8b59e7ae0fb18211070f4645aa80
Zhao <30463691+Sniper970119@users.noreply.github.com>
2018-08-22 22:20:34 +0800

    updata

--- src/FindRepetition.java
+++ src/FindRepetition.java
@@ -7,7 +7,9 @@ import java.util.Scanner;
  */
 public class FindRepetition {
     public static void main(String[] args){
+        // 保存数据
         ArrayList<Integer> arrayList = new ArrayList<>();
+        // set去重，保存都有哪些数字
         HashSet<Integer> data = new HashSet<>();
         Scanner scanner = new Scanner(System.in);
         int all = 0;
@@ -17,13 +19,16 @@ public class FindRepetition {
             arrayList.add(temp);
             data.add(temp);
         }
+        // 第一层对set遍历，找出都有哪些数字
         for (Integer integer:data) {
             int temp = 0;
+            // 第二层对arraylist遍历，查找有多少个关键数字
             for (Integer i : arrayList) {
                 if (i==integer){
                     temp++;
                 }
             }
+            // 如果大于半数，输出，程序退出
             if (temp>=(all/2)){
                 System.out.println(integer);
                 System.exit(0);
