c9b61f9c070b3959c29b0a51dc8ad0e8fd8c01a5
Zhao <30463691+Sniper970119@users.noreply.github.com>
2018-08-22 21:52:39 +0800

    updata

--- src/OverturnTheNumber.java
+++ src/OverturnTheNumber.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
+
 /**
  * Created by Sniper on 2018/8/22.
  */
 public class OverturnTheNumber {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        int result = overturn(overturn(m)+overturn(n));
+        System.out.println(result);
 
+    }
+
+    /**
+     * 计算数字的翻转
+     * @param number : 需要被翻转的数字
+     * @return :翻转之后的数字，返回int类型。
+     * */
+    private static int overturn(int number){
+        String words = ""+number;
+        String tempOne = "";
+        for (int i = words.length()-1; i >=0; i--) {
+            tempOne = tempOne + words.charAt(i);
+        }
+        try {
+            return Integer.parseInt(tempOne);
+        }catch (NumberFormatException e){
+            System.out.println("Error");
+            System.exit(0);
+        }
+        return 0;
     }
 }
