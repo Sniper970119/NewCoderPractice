d9e9d383821b7c16c3890aa302d69186bcafa03d
Zhao <30463691+Sniper970119@users.noreply.github.com>
2018-08-23 22:50:53 +0800

    updata

--- src/RestoreArray.java
+++ src/RestoreArray.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -16,19 +17,18 @@ public class RestoreArray {
                 flag[a[i]] = true;
             }
         }
+        // 把没有使用过的数字放到列表中，供后面全排列用
         ArrayList<Integer> arrayList = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
             if (!flag[i]){
                 arrayList.add(i);
             }
         }
+        // list用来保存arraylist 变量，保存列表的列表，保存全排列的结果
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         computeString(list,arrayList,0);
-//        for (ArrayList<Integer> i:list) {
-//            System.out.println(i.get(0));
-//        }
 
-        // 使用全排列计算完整数组
+        // 使用全排列计算完整数组 eg[4 1 3 2 5]
         int[] temp;
         ArrayList<Integer> tempArray;
         int index;
@@ -36,7 +36,6 @@ public class RestoreArray {
         for (int i = 0; i < list.size(); i++) {
             temp = Arrays.copyOf(a,n);
             tempArray = list.get(i);
-//            System.out.println(tempArray);
             index = 0;
             // 填充数组，使数组全为非0数
             for (int j = 0; j < n; j++) {
@@ -45,49 +44,55 @@ public class RestoreArray {
                     index++;
                 }
             }
-//            for (int zz = 0; zz < a.length; zz++) {
-//                System.out.print(a[zz]+" ");
-//            }
-            System.out.println();
+            // 如果满足k组排列
             if (judgeOrder(temp,k)){
                 result++;
             }
         }
         System.out.println(result);
     }
+    /**
+     * 判断排序是否正确，即是否有需要组排列
+     * @param data 需要被判断的数组
+     * @param n 判断条件，需要有n组排列
+     * @return 是否符合要求，符合返回true
+     * */
     private static boolean judgeOrder(int[] data, int n){
         int result = 0;
-        for (int i = 0; i < data.length; i++) {
-            System.out.print(data[i]+" ");
-        }
-        System.out.println();
+        // 判断排序组数
         for (int i = 0; i < data.length; i++) {
             for (int j = i; j < data.length; j++) {
                 if (data[i]<data[j]){
-                System.out.println(data[i]+" "+data[j]);
                     result++;
                 }
             }
 
         }
+        // 如果排列组数和需求组数一样，则返回true
         if (result==n){
-            System.out.println("true");
             return true;
         }else {
-            System.out.println("false");
             return false;
         }
 
     }
+    /**
+     * 计算数组的全排列
+     * @param arrayList 需要被全排列的列表
+     * @param list 保存全排列的列表
+     * @param n 索引，用来控制从某一位开始交换
+     * */
     private static void computeString (List<ArrayList<Integer>> list, ArrayList<Integer> arrayList, int n){
+        //如果已经交换到了最后一位，即和长度相同
         if (n==arrayList.size()) {
-//            System.out.println(arrayList);
-            list.add(new ArrayList<>(arrayList));
+            // 去重判断，防止重复计算
+            if (!list.contains(arrayList)){
+                list.add(new ArrayList<>(arrayList));
+            }
         }else {
+            // 遍历交换数据
             for (int i = 0; i < arrayList.size(); i++) {
-//                System.out.println("1:"+arrayList+" n: "+n);
                 Collections.swap(arrayList,i,n);
-//                System.out.println(arrayList);
                 computeString(list, arrayList, n + 1);
                 Collections.swap(arrayList,n,i);
             }
