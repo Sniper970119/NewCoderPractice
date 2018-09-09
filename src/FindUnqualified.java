8873e60012db4280f224a1bc85b9f45fc012d4a1
Zhao <30463691+Sniper970119@users.noreply.github.com>
2018-09-09 19:58:07 +0800

    updata

--- src/FindUnqualified.java
+++ src/FindUnqualified.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+
 /**
  * Created by Sniper on 2018/9/9.
  */
 public class FindUnqualified {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+        int[] z = new int[n];
+        for (int i = 0; i < n; i++) {
+           x[i] = scanner.nextInt();
+           y[i] = scanner.nextInt();
+           z[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(x[i]);
+            System.out.print(y[i]);
+            System.out.println(z[i]);
+        }
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (x[i] < x[j] && y[i] < y[j] && z[i] < z[j]) {
+//                    System.out.println("one");
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
     }
 }
